806147970d49fb2574068e316f12e7ca8ac82603
CaCackCack <cacackcack@protonmail.ch>
2022-01-08 01:31:16 -0500

    The necessary screen test.

---
+++ main.py
@@ -0,0 +1,22 @@
+# Project #2
+# See requirements in gClassroom
+# Clear and readable code is the best code
+# Student Name: Jayden Diaz
+
+### DEPENDENCIES ###
+import pygame
+import sys, math
+
+pygame.init()
+
+### CONSTANTS ###
+
+WIDTH, HEIGHT = 800, 600
+SIZE = (WIDTH, HEIGHT)
+
+## Main code
+
+# We start with a 2D base, the 3D comes with a little math
+
+screen = pygame.display.set_mode(SIZE)
+
